459b70f4820bcc587b848283ef33cb386cc1f5da
AnnLoseva <ann.loseva03@gmail.com>
2025-06-01 19:26:13 +0300

    Animation

--- Farm/Assets/Skripts/Building.cs
+++ Farm/Assets/Skripts/Building.cs
@@ -8,39 +8,26 @@ using Debug = UnityEngine.Debug;
 public class Building : MonoBehaviour
 {
     [SerializeField] private int price;
-    [SerializeField] float buildTime;
     [SerializeField] private Sprite emptySprite;
-    [SerializeField] private Sprite buildingSprite;
     [SerializeField] private Sprite readySprite;
 
-    private bool isBuilding;
     private bool isBuild;
     private SpriteRenderer spriteRenderer;
-    private float currentBuildingTime;
+    private Animator animator;
 
     private void Start()
     {
+        animator = GetComponent<Animator>();
         spriteRenderer = GetComponent<SpriteRenderer>();
         spriteRenderer.sprite = emptySprite;
         ChangeColider();
     }
 
-    private void Update()
-    {
-        if (isBuilding)
-        {
-            currentBuildingTime += Time.deltaTime;
-            if(currentBuildingTime > buildTime)
-            {
-                FinishBuilding();
-            }
-        }
-    }
 
-    public int Click(int money)
+    public int Click(int money) // Любой клик по зданию
     {
 
-        if (!isBuild & !isBuilding)
+        if (!isBuild )
         {
             money = Build(money);
 
@@ -53,15 +40,13 @@ public class Building : MonoBehaviour
         return money;
     }
 
-    private int Build(int money)
+    private int Build(int money) //Начало стройки, вычитание цены от суммы
     {
         if (money >= price)
         {
             Debug.Log(money);
             money -= price;
-            spriteRenderer.sprite = buildingSprite;
-            isBuilding = true;
-            
+            animator.Play("Building Works");
 
         }
 
@@ -69,17 +54,15 @@ public class Building : MonoBehaviour
 
     }
 
-    private void FinishBuilding()
+    private void FinishBuilding() //Завершение стройки послек того, как прошла стройка
     {
-        isBuilding = false;
-        currentBuildingTime = 0;
         isBuild = true;
         spriteRenderer.sprite = readySprite;
         ChangeColider();
      
     }
 
-    private int Destroy(int money)
+    private int Destroy(int money) // Разрушение объекта, возвращение денег
     {
         money += price;
         isBuild = false;
@@ -89,7 +72,9 @@ public class Building : MonoBehaviour
         return money; 
     }
 
-    private void ChangeColider()
+
+
+    private void ChangeColider()  //Смена коллайдера при смене спрайта
     {
         PolygonCollider2D collider = GetComponent<PolygonCollider2D>();
 
